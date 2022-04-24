621eec97ede44e2420c4417326346ac1e17effec
BADHAN SEN <96924254+Badhon9080@users.noreply.github.com>
2022-04-24 06:02:45 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,32 @@
+//var num = 800;
+
+ //if (num >= 80 && num <= 100) {
+ //  document.write("I am GPA-5");
+ //} else {
+ //  document.write("Jibon Sesh");
+/// }
+
+// const pi = 3.1416;
+
+// function sum(a,b,c) {
+//    return a + b + c + "<br>"
+// }
+
+// alert(sum(4, 4,5))
+// document.write(sum(4, 4,5))
+// sum(3,3,4)
+// sum(20,4,3)
+// sum(60,40,20)
+// sum(25, 35, 17)
+
+// function substract(a, b) {
+//    document.write(a-b)
+// }
+
+// substract(8,3)
+
+//loop(for,while,do while)
+
+for (let i = 9; i <=  100; ++i) {
+    document.write(i + '<br>');
+ }
